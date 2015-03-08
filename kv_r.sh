def91b16fcf2232d68fb33d027fa75b8249f88cf
dianagorun <d.gorun@yahoo.com>
2015-03-07 17:58:43 -0800

    ...

--- kv_r.sh
+++ kv_r.sh
@@ -2,7 +2,7 @@ rpcgen kv.x
 gcc -c kv_xdr.c
 gcc -c kv_svc.c
 gcc -c kv_clnt.c
-gcc -pthread -o kv_proc kv_proc_r.c
+gcc -c kv_clnt.o kv_xdr.o kv_proc_r.c -lpthread -lm
 gcc -c rkv.c
 gcc -o server kv_xdr.o kv_svc.o kv_proc_r.o kv_clnt.o
 gcc -o client kv_xdr.o kv_clnt.o rkv.o
