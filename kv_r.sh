5322616e66aafc6ad7bda499b4b0932c4e51cb56
dianagorun <d.gorun@yahoo.com>
2015-03-07 17:13:35 -0800

    ..

--- kv_r.sh
+++ kv_r.sh
@@ -4,5 +4,5 @@ cc -c kv_svc.c
 cc -c kv_clnt.c
 cc -pthread -o kv_proc kv_proc_r.c -lm
 cc -c rkv.c
-cc -o server kv_xdr.o kv_svc.o kv_proc_r.o kv_clnt.o
-cc -o client kv_xdr.o kv_clnt.o rkv.o
+cc -o server kv_xdr.o kv_svc.o kv_proc_r.o kv_clnt.o -lm
+cc -o client kv_xdr.o kv_clnt.o rkv.o -lm
