2059970d86ef5801f32c61189dd0f7b6b6fe9aed
dianagorun <d.gorun@yahoo.com>
2015-03-07 17:34:39 -0800

    …

--- kv_r.sh
+++ kv_r.sh
@@ -1,8 +1,8 @@
 rpcgen kv.x
-cc -c kv_xdr.c
-cc -c kv_svc.c
-cc -c kv_clnt.c
-cc -pthread -o kv_proc kv_proc_r.c
-cc -c rkv.c
-cc -o server kv_xdr.o kv_svc.o kv_proc_r.o kv_clnt.o
-cc -o client kv_xdr.o kv_clnt.o rkv.o
+gcc -c kv_xdr.c
+gcc -c kv_svc.c
+gcc -c kv_clnt.c
+gcc -pthread -o kv_proc kv_proc_r.c
+gcc -c rkv.c
+gcc -o server kv_xdr.o kv_svc.o kv_proc_r.o kv_clnt.o
+gcc -o client kv_xdr.o kv_clnt.o rkv.o
