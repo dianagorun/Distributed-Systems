6c358f789e3e2ec3b2a6c538bb57c1e70724b251
dianagorun <d.gorun@yahoo.com>
2015-03-07 18:04:01 -0800

    …

--- kv_r.sh
+++ kv_r.sh
@@ -5,3 +5,4 @@ gcc -c kv_clnt.c
 cc -c kv_clnt.o kv_xdr.o kv_proc_r.c -lpthread -lm
 gcc -c rkv.c
 gcc -o server kv_xdr.o kv_svc.o kv_proc_r.o kv_clnt.o -lpthread -lm
+gcc -o client kv_xdr.o kv_clnt.o rkv.o
